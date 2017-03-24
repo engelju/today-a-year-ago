499fc20b30691e9fa8c8ce081ecfec0d22743357
Julie Engel <jue@metanet.ch>
2017-03-24 17:19:52 +0100

    remove unneeded css

--- public/index.php
+++ public/index.php
@@ -14,27 +14,10 @@
 <body>
     <nav class="navbar navbar-default">
         <div class="container-fluid">
-            <!-- Brand and toggle get grouped for better mobile display -->
             <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
                 <a class="navbar-brand" href="#/">Today, a year ago</a>
             </div>
-
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <!--form class="navbar-form navbar-right" role="search">
-                    <div class="form-group">
-                        <input type="text" class="form-control" placeholder="Search">
-                    </div>
-                    <button type="submit" class="btn btn-default">Submit</button>
-                </form-->
-            </div><!-- /.navbar-collapse -->
-        </div><!-- /.container-fluid -->
+        </div>
     </nav>
 
     <div id="container" class="container">
